c4c091a5a89f4358672f2d0d2a60648fe5286ef9
nileshpandey31 <nileshpandey786786@gmail.com>
2021-03-30 23:39:01 +0530

    homepageadded

--- OnlineExaminationSystem/src/app/app.module.ts
+++ OnlineExaminationSystem/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './Components/home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
