89a1f1f4083e01f90a99b9bb7660c0333ff9ac59
Sunil <36020672+sudo-sunil@users.noreply.github.com>
2021-03-31 10:55:24 +0530

    registration page added

--- OnlineExaminationSystem/src/app/app.module.ts
+++ OnlineExaminationSystem/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './Components/home-page/home-page.component';
+import { RegisterPageComponent } from './Components/register-page/register-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent
+    HomePageComponent,
+    RegisterPageComponent
   ],
   imports: [
     BrowserModule,
