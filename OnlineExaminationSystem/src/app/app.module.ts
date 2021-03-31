4f49813f32ee56b8c759acb68ac90f07ae987b1e
nileshpandey31 <nileshpandey786786@gmail.com>
2021-03-31 16:46:23 +0530

    login resolved

--- OnlineExaminationSystem/src/app/app.module.ts
+++ OnlineExaminationSystem/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './Components/home-page/home-page.component';
 import { RegisterPageComponent } from './Components/register-page/register-page.component';
+import {LoginComponent} from './Components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
-    RegisterPageComponent
+    RegisterPageComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
