9a79140b1eb0f58b6df7d3a7d6c6cb44559d6204
nileshpandey31 <nileshpandey786786@gmail.com>
2021-04-07 15:01:04 +0530

    studentinfo module created

---
+++ OnlineExaminationSystem/src/app/Modules/student-info/student-info.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class StudentInfoModule { 
+ StudentId:number;
+ Name:string;
+ Mobile:string;
+ Email:string;
+ DOB:Date;
+ College:string;
+ City:string;
+ State:string;
+ Qualification:string;
+ YearOfCompletion:string;
+ Password:string;
+  LastLogin:Date;
+}
