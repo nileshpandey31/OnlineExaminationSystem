ee661e14867e75622a9354a67a85fac6248a95a0
nileshpandey31 <nileshpandey786786@gmail.com>
2021-04-08 18:34:52 +0530

    commit

--- OnlineExamAPI/OnlineExamAPI/Models/Model1.Designer.cs
+++ OnlineExamAPI/OnlineExamAPI/Models/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\LTI training\FinalProject\OnlineExamAPI\OnlineExamAPI\Models\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\nilesh\Desktop\LTI FINAL PROJECT\OnlineExaminationSystem\OnlineExamAPI\OnlineExamAPI\Models\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
