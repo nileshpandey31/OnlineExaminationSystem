6df71c12310ee073ad3b123bd58bab1e0bc675be
nileshpandey31 <nileshpandey786786@gmail.com>
2021-04-19 13:22:28 +0530

    search uodated

---
+++ OnlineExaminationSystem/src/app/Modules/select-student/select-student.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class SelectStudentModule {
+  StudentId:number;
+  Name:string;
+  Email:string;
+  Subject:string;
+  Level:number;
+  Marks:number;
+  
+  //Id :number;
+ Technology:string ='';
+ state:string='';
+ city:any='';
+
+
+ }
